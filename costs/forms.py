a4bcf581d73d2e2826e68898ac0731aa4098b5ef
datagutten <datagutten@datagutten.net>
2020-06-19 20:52:12 +0200

    Disable user selection, it makes the form too slow to load

--- costs/forms.py
+++ costs/forms.py
@@ -6,4 +6,6 @@ from costs.models import Application
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
-        fields = '__all__'
+        # TODO: Rewrite user selection with AJAX autocomplete
+        # <select> elements makes the load too slow
+        exclude = ['responsible', 'super_user']
