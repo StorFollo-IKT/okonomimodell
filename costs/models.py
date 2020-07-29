fab8e9c87c1e7dd57bd09e01950e36e60d187b03
datagutten <datagutten@datagutten.net>
2020-07-29 16:20:23 +0200

    Make DN field for servers optional and add field for imported (write protect)

--- costs/models.py
+++ costs/models.py
@@ -51,12 +51,14 @@ class Product(models.Model):
 class Server(models.Model):
     name = models.CharField('Navn', max_length=50)
     dns_name = models.CharField('DNS-navn', max_length=100, null=True, blank=True)
-    dn = models.CharField('DN', max_length=200)
+    dn = models.CharField('DN', max_length=200, null=True, blank=True)
     description = models.CharField('Beskrivelse', max_length=100, null=True, blank=True)
     last_logon = models.DateTimeField('Sist aktiv', null=True, blank=True)
     last_update = models.DateTimeField('Sist oppdatert', auto_now=True)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='servers', verbose_name='Tjeneste', null=True)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='servers', verbose_name='Tjeneste',
+                                null=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT, verbose_name='Kunde', related_name='servers')
+    imported = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ['name', 'customer']
