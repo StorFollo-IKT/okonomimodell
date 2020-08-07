1461661542af04a9ff4c408b52bae5a57d5da07e
Knut Haug <hauknu@gmail.com>
2020-08-07 14:57:01 +0200

    Added footer summation to Portfolio tables, started moving javascript to separate file

---
+++ costs/static/costs/table.js
@@ -0,0 +1,20 @@
+     function footerCallback( row, data, start, end, display ) {
+                var api = this.api(), data;
+                // Remove the formatting to get integer data for summation
+                var intVal = function ( i ) {
+                    return typeof i === 'string' ?
+                        i.replace(/\D/g, '')*1 :
+                        typeof i === 'number' ?
+                            i : 0;
+                };
+
+                api.columns('.sum', { page: 'current'}).every( function () {
+                  var sum = this
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+
+                  this.footer().innerHTML = sum.toLocaleString().replace(',',' ') + ' kr';
+                } );
+            }
