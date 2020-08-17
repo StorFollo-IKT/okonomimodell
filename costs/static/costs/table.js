856875ea96e863f079f78d5a0d204b9a28e8095c
Knut Haug <hauknu@gmail.com>
2020-08-17 12:35:48 +0200

    Added total yearly sum across tables for portfolio and costs_report

--- costs/static/costs/table.js
+++ costs/static/costs/table.js
@@ -1,20 +1,30 @@
-     function footerCallback( row, data, start, end, display ) {
-                var api = this.api(), data;
-                // Remove the formatting to get integer data for summation
-                var intVal = function ( i ) {
-                    return typeof i === 'string' ?
-                        i.replace(/\D/g, '')*1 :
-                        typeof i === 'number' ?
-                            i : 0;
-                };
+function footerCallback(row, data, start, end, display) {
+    var api = this.api(), data;
+    // Remove the formatting to get integer data for summation
 
-                api.columns('.sum', { page: 'current'}).every( function () {
-                  var sum = this
-                    .data()
-                    .reduce( function (a, b) {
-                        return intVal(a) + intVal(b);
-                    }, 0 );
+    api.columns('.sum', {page: 'current'}).every(function () {
+        var sum = this
+            .data()
+            .reduce(function (a, b) {
+                return intVal(a) + intVal(b);
+            }, 0);
 
-                  this.footer().innerHTML = sum.toLocaleString().replace(/,/g,' ') + ' kr';
-                } );
-            }
+        this.footer().innerHTML = sum.toLocaleString().replace(/,/g, ' ') + ' kr';
+    });
+
+}
+
+function intVal(i) {
+    return typeof i === 'string' ?
+        i.replace(/\D/g, '') * 1 :
+        typeof i === 'number' ?
+            i : 0;
+};
+
+function calculateYearlyTotal() {
+    var sum = 0;
+    $('.yearlyTotal').each(function () {
+        sum += intVal($(this).text());
+    });
+    $('#totalSum').text(sum.toLocaleString().replace(/,/g, ' ') + ' kr');
+}
