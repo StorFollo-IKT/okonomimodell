2f2ba76255f3a053920eea8e3e44347986fd5e7d
Anders Birkenes <datagutten@datagutten.net>
2020-09-24 13:38:33 +0200

    Add total sum for ProductGroup

--- costs/models.py
+++ costs/models.py
@@ -304,6 +304,13 @@ class ProductGroup(models.Model):
     name = models.CharField(max_length=100)
     products = models.ManyToManyField(Product, verbose_name='Tjenester', related_name='product_groups')
 
+    def sum_year(self):
+        total = 0
+        for line in self.products.all():
+            total += line.sum_year()
+
+        return total
+
     class Meta:
         verbose_name = 'Tjenestegruppe'
         verbose_name_plural = 'Tjenestegrupper'
