dbba194ce712f33971074a54f8a58bea7f7bf214
datagutten <datagutten@datagutten.net>
2020-09-25 13:46:50 +0200

    Add error handling

--- costs/management/commands/match_users.py
+++ costs/management/commands/match_users.py
@@ -13,13 +13,28 @@ now = datetime.datetime.now()
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in User.objects.all():
+            if not user.customer:
+                print('%s has no customer' % user)
+                continue
+
             company = user.customer.id
+            if not user.ad_object:
+                print('%s has no AD object' % user)
+                continue
+
             if not user.ad_object.employeeID:
                 continue
+
+            try:
+                resource_id = int(user.ad_object.employeeID)
+            except ValueError:
+                print('Invalid resource ID for %s: %s' % (user, user.ad_object.employeeID))
+                continue
+
             try:
                 employee = Resource.objects.get(company__companyCode=company,
-                                                resourceId=user.ad_object.employeeID)
+                                                resourceId=resource_id)
                 user.employee = employee
                 user.save()
             except Resource.DoesNotExist:
-                print('No resource in company %s with resourceId %s' % (company, user.number))
+                print('No resource in company %s with resourceId %s' % (company, resource_id))
