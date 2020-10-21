02db3446dbf3c244eb54fdc96c2065da18aa4027
datagutten <datagutten@datagutten.net>
2020-10-21 10:05:02 +0200

    Add IP to server and allow ad_object to be blank

--- costs/models.py
+++ costs/models.py
@@ -71,6 +71,7 @@ class Server(models.Model):
     name = models.CharField('Navn', max_length=50)
     dns_name = models.CharField('DNS-navn', max_length=100, null=True, blank=True)
     dn = models.CharField('DN', max_length=200, null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True, default=None)
     description = models.CharField('Beskrivelse', max_length=100, null=True, blank=True)
     last_logon = models.DateTimeField('Sist aktiv', null=True, blank=True)
     last_update = models.DateTimeField('Sist oppdatert', auto_now=True)
@@ -78,7 +79,7 @@ class Server(models.Model):
                                 null=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT, verbose_name='Kunde', related_name='servers')
     imported = models.BooleanField(default=False)
-    ad_object = models.OneToOneField(ADServer, on_delete=models.CASCADE, null=True, default=None, verbose_name='AD')
+    ad_object = models.OneToOneField(ADServer, on_delete=models.CASCADE, null=True, blank=True, default=None, verbose_name='AD')
 
     class Meta:
         unique_together = ['name', 'customer']
