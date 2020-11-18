ef96c4310af3cc8bf7922a42ca1da8efefe8e9f0
datagutten <datagutten@datagutten.net>
2020-11-18 19:10:46 +0100

    Remove sector method from Department Replaced by relation between Application and Sector

--- costs/models/Department.py
+++ costs/models/Department.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import MultipleObjectsReturned
 from django.db import models
 
 from costs.models import Customer
@@ -17,14 +16,3 @@ class Department(models.Model):
 
     def __str__(self):
         return '%s (%s)' % (self.name, self.customer)
-
-    def sector(self):  # TODO: Rewrite all references to this to application.sector
-        try:
-            return self.sector_dep.get(departments__number=self.number)
-        except MultipleObjectsReturned as e:
-            print(e)
-            print('Department %s matched multiple sectors:' % self)
-            sectors = self.sector_dep.filter(departments__number=self.number)
-            for sector in sectors:
-                print(sector)
-            return sectors.first()
