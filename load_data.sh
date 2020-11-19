adfaac75b49716e505a339e35cae2e0d50f54e0d
datagutten <datagutten@datagutten.net>
2020-11-19 10:02:43 +0100

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -5,5 +5,8 @@ BASEDIR=$(dirname "$0")
 /usr/bin/python3 "$BASEDIR/manage.py" load_organisations all
 /usr/bin/python3 "$BASEDIR/manage.py" load_resources all
 
-/usr/bin/python3 "$BASEDIR/manage.py" load_users all
+/usr/bin/python3 "$BASEDIR/manage.py" load_users
 /usr/bin/python3 "$BASEDIR/manage.py" match_users
+
+/usr/bin/python3 "$BASEDIR/manage.py" load_workstations
+/usr/bin/python3 "$BASEDIR/manage.py" load_servers
