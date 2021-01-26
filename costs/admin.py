5dba4e67677a886efee74b04039ef5155c16bc8d
datagutten <datagutten@datagutten.net>
2021-01-26 10:01:57 +0100

    Add more fields to workstation admin

--- costs/admin.py
+++ costs/admin.py
@@ -81,8 +81,10 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Workstation)
 class WorkstationAdmin(admin.ModelAdmin):
     list_select_related = ['ad_object']
-    list_display = ['name', 'manufacturer', 'model', 'owner_display_name', 'user_display_name']
-    list_filter = ['ad_object__directory', 'ad_object__lastLogon', 'customer', 'manufacturer', 'model']
+    list_display = ['name', 'manufacturer', 'model', 'owner_display_name', 'user_display_name', 'cost_center',
+                    'function', 'last_logon', 'last_logon_ad']
+    list_filter = ['ad_object__directory', 'ad_object__lastLogon', 'ad_object__operatingSystem', 'customer',
+                   'manufacturer', 'model']
     readonly_fields = ['ad_object', 'user']
 
 
